c534f0789ffc088934352ebca19cc0da542565d7
Sdcarter95 <91804096+Sdcarter95@users.noreply.github.com>
2023-10-22 01:08:14 -0500

    Configure  swiper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.png';
 import './App.css';
 import CassetteCarousel from './Components/CassetteCarousel';
@@ -12,23 +12,33 @@ export interface Cassette {
 function App() {
   const [videoSource, setVideoSource] = useState<string>("https://www.youtube.com/embed/52FljdTl2_M?si=LiZyMDhhxgWG55Ul");
   const [cassetteLibrary, setCassetteLibrary] = useState<Cassette[]>([]);
-  
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const cassetteSFX = "/cassestSound.mp3";
+
   useEffect(() => {
     let newLibrary: Cassette[] = [];
-    newLibrary.push({name: "frogy", source: "https://www.youtube.com/embed/52FljdTl2_M?si=LiZyMDhhxgWG55Ul", video_id: "52FljdTl2_M"});
-    newLibrary.push({name: "90s", source: "https://www.youtube.com/embed/GabqgJEeigs?si=_nX614lTGDLrBRbF", video_id: "GabqgJEeigs"});
-    newLibrary.push({name: "lofi girl", source: "https://www.youtube.com/embed/jfKfPfyJRdk?si=wNwSN9cngcAIGomi", video_id: "jfKfPfyJRdk"});
-    newLibrary.push({name: "relax", source: "https://www.youtube.com/embed/851FQiikDaw?si=M6O6JksolCvMFyvS", video_id: "851FQiikDaw"});
-    newLibrary.push({name: "neo jazz", source: "https://www.youtube.com/embed/CE8mevzFwO0?si=vL1j-eVdpRerRUnX", video_id: "CE8mevzFwO0"});
-    newLibrary.push({name: "rainy cafe", source: "https://www.youtube.com/embed/NJuSStkIZBg?si=V7NWkkCDeNdmTAOO", video_id: "NJuSStkIZBg"});
-    newLibrary.push({name: "weeds", source: "https://www.youtube.com/embed/_yJu15Qq3To?si=Ka9j5Ry-q_4CCpk3", video_id: "_yJu15Qq3To"});
+    newLibrary.push({ name: "frogy", source: "https://www.youtube.com/embed/52FljdTl2_M?si=LiZyMDhhxgWG55Ul", video_id: "52FljdTl2_M" });
+    newLibrary.push({ name: "90s", source: "https://www.youtube.com/embed/GabqgJEeigs?si=_nX614lTGDLrBRbF", video_id: "GabqgJEeigs" });
+    newLibrary.push({ name: "lofi girl", source: "https://www.youtube.com/embed/jfKfPfyJRdk?si=wNwSN9cngcAIGomi", video_id: "jfKfPfyJRdk" });
+    newLibrary.push({ name: "relax", source: "https://www.youtube.com/embed/851FQiikDaw?si=M6O6JksolCvMFyvS", video_id: "851FQiikDaw" });
+    newLibrary.push({ name: "neo jazz", source: "https://www.youtube.com/embed/CE8mevzFwO0?si=vL1j-eVdpRerRUnX", video_id: "CE8mevzFwO0" });
+    newLibrary.push({ name: "rainy cafe", source: "https://www.youtube.com/embed/NJuSStkIZBg?si=V7NWkkCDeNdmTAOO", video_id: "NJuSStkIZBg" });
+    newLibrary.push({ name: "weeds", source: "https://www.youtube.com/embed/_yJu15Qq3To?si=Ka9j5Ry-q_4CCpk3", video_id: "_yJu15Qq3To" });
     setCassetteLibrary(newLibrary);
   }, [])
 
   const handleSlideClick = (cassette: Cassette) => {
+    playAudio();
     setVideoSource(cassette.source);
+
   };
-  
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
 
   return (
     <div className="App">
@@ -38,8 +48,12 @@ function App() {
           “Opportunities don't happen, you create them."
         </p>
       </header>
-      <CassetteCarousel cassettes={cassetteLibrary} onSlideClick = {handleSlideClick}/>
-      <iframe className='Frame' width="560" height="315" src={videoSource} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <CassetteCarousel cassettes={cassetteLibrary} onSlideClick={handleSlideClick} />
+      <audio ref={audioRef}>
+        <source src={cassetteSFX} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <iframe className='Frame' width="560" height="315" src={videoSource + "?autoplay=1"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   );
 }
--- src/Components/CassetteCarousel.tsx
+++ src/Components/CassetteCarousel.tsx
@@ -4,28 +4,39 @@ import { Cassette } from '../App';
 import 'swiper/css';
 import './css/CassetteCarousel.css'
 
+
 interface CarouselProps {
   cassettes: Cassette[];
   onSlideClick: (cassette: Cassette) => void;
 }
 
 const CassetteCarousel: React.FC<CarouselProps> = ({ cassettes, onSlideClick }) => {
+  const swiperParams = {
+    spaceBetween: 20,
+    slidesPerView: 4,
+    centeredSlides: true,
+    loop: false,
+    slideToClickedSlide: true,
+  };
+
   
+
   return (
-    <Swiper
-      spaceBetween={20} 
-      slidesPerView={3} 
-      centeredSlides={true} 
-      loop={true} 
-    >
-      {cassettes.map((cassette, index) => (
-        <SwiperSlide key={index}>
-          <div className="slide-content" onClick={() => onSlideClick(cassette)}>
-            <img src={`https://img.youtube.com/vi/${cassette.video_id}/maxresdefault.jpg`} alt={cassette.name} />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="swiper-container">
+      <Swiper {...swiperParams}>
+        {cassettes.map((cassette, index) => (
+          <SwiperSlide key={index}>
+            <div className="slide-content" onClick={() => onSlideClick(cassette)}>
+              <img src={`https://img.youtube.com/vi/${cassette.video_id}/maxresdefault.jpg`} alt={cassette.name} />
+            </div>
+          </SwiperSlide>
+        ))}
+        <div className="swiper-button-prev"></div> 
+        <div className="swiper-button-next"></div> 
+      </Swiper>
+
+    </div>
+
   );
 };
 
