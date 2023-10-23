e473242f38bde12cb2da696e6dd08e0a1f5bf2c3
Sdcarter95 <91804096+Sdcarter95@users.noreply.github.com>
2023-10-22 22:51:34 -0500

    Add Open Cassette Library transition

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.png';
 import './App.css';
 import CassetteCarousel from './Components/CassetteCarousel';
-import AudioPlayer from './Components/SoundEffect';
 
 export interface Cassette {
   name: string,
@@ -12,9 +11,13 @@ export interface Cassette {
 
 function App() {
   const [videoSource, setVideoSource] = useState<string>("https://www.youtube.com/embed/851FQiikDaw?si=M6O6JksolCvMFyvS");
+  const [displayImage, setDisplayImage] = useState<string>("");
   const [cassetteLibrary, setCassetteLibrary] = useState<Cassette[]>([]);
+  const [quoteBook, setQuoteBook] = useState<string[]>([]);
+  const [quote, setQuote] = useState<string>("");
+  const [cassetteSelectionVisible, setCassetteSelectionVisible] = useState<boolean>(false);
   const audioRef = useRef<HTMLAudioElement | null>(null);
-  
+
 
 
   useEffect(() => {
@@ -27,14 +30,26 @@ function App() {
     newLibrary.push({ name: "rainy cafe", source: "https://www.youtube.com/embed/NJuSStkIZBg?si=V7NWkkCDeNdmTAOO", video_id: "NJuSStkIZBg" });
     newLibrary.push({ name: "weeds", source: "https://www.youtube.com/embed/_yJu15Qq3To?si=Ka9j5Ry-q_4CCpk3", video_id: "_yJu15Qq3To" });
     setCassetteLibrary(newLibrary);
-  }, [])
+    let newQuoteBook: string[] = [];
+    newQuoteBook.push(`“Opportunities don't happen, you create them.”`);
+    newQuoteBook.push(`“One sees in the world what they carry in their heart”`);
+    newQuoteBook.push("“Doubt kills more dreams than failure ever will”");
+    setQuoteBook(newQuoteBook);
+  }, []);
+
+  useEffect(() => {
+    const randomQIndex = Math.floor(Math.random() * quoteBook.length);
+    setQuote(quoteBook[randomQIndex]);
+  }, [quoteBook]);
+
 
   const handleSlideClick = (cassette: Cassette) => {
     setVideoSource(cassette.source);
+    //setDisplayImage(`https://img.youtube.com/vi/${cassette.video_id}/maxresdefault.jpg`);
     playAudio();
   };
 
- 
+
   const playAudio = () => {
     if (audioRef.current) {
       audioRef.current.play();
@@ -42,21 +57,37 @@ function App() {
   };
 
 
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <audio ref={audioRef}>
-        <source src={"https://docs.google.com/uc?export=download&id=1XtWv60ze6CtM-geWnYHf3owgZ21URd4H"} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+          <source src={"https://docs.google.com/uc?export=download&id=1XtWv60ze6CtM-geWnYHf3owgZ21URd4H"} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
         <p className='Quote'>
-          “Opportunities don't happen, you create them."
+          {quote}
         </p>
       </header>
-      <CassetteCarousel cassettes={cassetteLibrary} onSlideClick={handleSlideClick} />
-      <iframe className='Frame' width="560" height="315" src={videoSource + "?autoplay=1"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <div className="App-body" style={{ backgroundImage: `url(${displayImage})` }}>
+        <div className='flex-container'>
+          <div className='flex-item'>
+          </div>
+          <div className='flex-item'>
+            <div className='CassettePlayer'>
+              {cassetteSelectionVisible ? <CassetteCarousel cassettes={cassetteLibrary} onSlideClick={handleSlideClick} />:<></>}
+              {cassetteSelectionVisible ?
+                <iframe className='Frame-Down' width="560" height="315" src={videoSource + "?autoplay=1"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> 
+                :<iframe className='Frame-Up' width="560" height="315" src={videoSource + "?autoplay=1"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> 
+              }
+              <button className='EjectButton' onClick={() => setCassetteSelectionVisible(!cassetteSelectionVisible)}>Eject</button>
+            </div>
+          </div>
+          <div className='flex-item'>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
