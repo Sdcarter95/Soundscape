d8c43786cda2644c5506758a9a922c2ba222002d
Sdcarter95 <91804096+Sdcarter95@users.noreply.github.com>
2023-10-28 14:44:28 -0500

    Refactor SoundConsole

--- src/Components/SoundConsole.tsx
+++ src/Components/SoundConsole.tsx
@@ -4,10 +4,24 @@ import { Howl, Howler } from 'howler';
 
 enum soundPaths {
     rain = "https://audio.jukehost.co.uk/TvTHAG7RVMB7eDb68O6Vmv4hKaBy6EkI",
+    fireplace = "https://audio.jukehost.co.uk/g0EDzD4kBGW8hB5Ahta27C6u6wtxHjj4",
+    train = "https://audio.jukehost.co.uk/GYo9wmXUMa3lAmznxmwXCXocACFMPs8t",
 }
 
 const rain_sound = new Howl({
     src: [soundPaths.rain],
+    html5: true,
+    loop: true,
+});
+
+const fire_sound = new Howl({
+    src: [soundPaths.fireplace],
+    html5: true,
+    loop: true,
+});
+
+const train_sound = new Howl({
+    src: [soundPaths.train],
     html5: true,
     loop: true,
 });
@@ -19,33 +33,32 @@ interface SoundConsoleProps {
 const SoundConsole: React.FC<SoundConsoleProps> = () => {
     const [masterVolume, setMasterVolume] = useState(100);
     const [rainVolume, setRainVolume] = useState(100);
+    const [fireVolume, setFireVolume] = useState(100);
+    const [trainVolume, setTrainVolume] = useState(100);
 
     useEffect(() => {
         setMasterVolume(Howler.volume() * 100);
         setRainVolume(rain_sound.volume() * 100);
+        setFireVolume(rain_sound.volume() * 100);
+        setTrainVolume(rain_sound.volume() * 100);
     }, []);
 
-    const handleMasterVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newVolume = parseInt(event.target.value, 10);
-        setMasterVolume(newVolume);
-        Howler.volume(newVolume / 100);
-    };
 
-    const handleRainVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>, sound: Howl|HowlerGlobal, setVolume: (volume: number) => void) => {
         const newVolume = parseInt(event.target.value, 10);
-        setRainVolume(newVolume);
-        rain_sound.volume(newVolume / 100);
+        setVolume(newVolume);
+        sound.volume(newVolume / 100);
     };
 
 
-
-    const handlePlay = () => {
-        if (rain_sound.playing()) {
-            rain_sound.pause();
+    const handlePlayToggle = (sound: Howl) => {
+        if (sound.playing()) {
+            sound.pause();
         } else {
-            rain_sound.play();
+            sound.play();
         }
     };
+
 
 
     return (
@@ -55,19 +68,40 @@ const SoundConsole: React.FC<SoundConsoleProps> = () => {
                 type="range"
                 min="0"
                 max="100"
-                value={rainVolume}
-                onChange={handleRainVolumeChange}
+                value={masterVolume}
+                onChange={(e) => handleVolumeChange(e, Howler, setMasterVolume)}
             />
             <br />
             <input
                 type="range"
                 min="0"
                 max="100"
-                value={masterVolume}
-                onChange={handleMasterVolumeChange}
+                value={rainVolume}
+                onChange={(e) => handleVolumeChange(e, rain_sound, setRainVolume)}
             />
-            <button onClick={handlePlay}>Rain</button>
+            <button onClick={()=>handlePlayToggle(rain_sound)}>Rain</button>
             <br />
+
+            <input
+                type="range"
+                min="0"
+                max="100"
+                value={fireVolume}
+                onChange={(e) => handleVolumeChange(e, fire_sound, setFireVolume)}
+            />
+            <button onClick={()=>handlePlayToggle(fire_sound)}>Fire</button>
+            <br />
+
+            <input
+                type="range"
+                min="0"
+                max="100"
+                value={trainVolume}
+                onChange={(e) => handleVolumeChange(e, train_sound, setTrainVolume)}
+            />
+            <button onClick={()=>handlePlayToggle(train_sound)}>Train</button>
+            <br />
+        
         </div>
     );
 }
