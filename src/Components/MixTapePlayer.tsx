e529cae1a206d4b993ea5c602d3e654a2373d49d
Sdcarter95 <91804096+Sdcarter95@users.noreply.github.com>
2023-11-06 16:11:09 -0600

    Update MixTapePlayer.tsx
    use flipper

--- src/Components/MixTapePlayer.tsx
+++ src/Components/MixTapePlayer.tsx
@@ -16,27 +16,58 @@ export interface Track {
 
 function MixTapePlayer({ tracks }: { tracks: Track[] }) {
   const [trackIndex, setTrackIndex] = useState<number>(0);
+  const [flipper, setFlipper] = useState(true);
+  const [tapeDone, setTapeDone] = useState(false);
+
+  useEffect(() => {
+    setTapeDone(false);
+    setFlipper(true);
+    setTrackIndex(0);
+  }, [tracks])
+
 
   const nextTrack = () => {
-    if (trackIndex + 2 <= tracks.length){
-      setTrackIndex(trackIndex + 1);
-    } 
+    if (trackIndex + 3 <= tracks.length) {
+      setTrackIndex(trackIndex + 2);
+      setFlipper(!flipper);
+    }
+    else { setTapeDone(true) }
   }
 
 
   return (
     <div>
-      <YouTube
-        videoId={tracks[trackIndex].src}
-        opts={{
-          playerVars: {
-            start: Math.floor(tracks[trackIndex].start),
-            end: Math.floor(tracks[trackIndex].end),
-            autoplay: 1,
-          },
-        }}
-        onEnd={nextTrack}
-      />
+      {tapeDone ? <></> :
+        <div>
+          {flipper ? <YouTube
+            videoId={tracks[trackIndex].src}
+            opts={{
+              playerVars: {
+                start: Math.floor(tracks[trackIndex].start),
+                end: Math.floor(tracks[trackIndex].end),
+                autoplay: 1,
+              },
+            }}
+            onEnd={() => setFlipper(!flipper)}
+          /> : <>there</>}
+
+          {!flipper && tracks[trackIndex + 1] ?
+            <YouTube
+              videoId={tracks[trackIndex + 1].src}
+              opts={{
+                playerVars: {
+                  start: Math.floor(tracks[trackIndex + 1].start),
+                  end: Math.floor(tracks[trackIndex + 1].end),
+                  autoplay: 1,
+                },
+              }}
+              onEnd={nextTrack}
+            /> : <></>
+          }
+        </div>
+      }
+
+
     </div>
   );
 };
